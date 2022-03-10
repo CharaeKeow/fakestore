738fee541ca488043d0825f5c5934e5202809c11
CharaeKeow <charae.ehsin@gmail.com>
2022-03-10 17:28:29 +0800

    add routing to a single product page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 
 import ProductList from './Components/ProductList';
+import Product from './Components/Product'
 
 /*
  React.PureComponent is better than Component in some cases due to it implements `shouldComponent()` by default (doing shallow comparison)
@@ -19,11 +20,11 @@ export default class App extends React.Component {
     return (
       <Router>
         <Routes>
-          <Route
-            exact
-            path="/"
-            element={<ProductList />}
-          />
+          <Route exact path="/" element={<ProductList />} />
+          <Route exact path="/products/:productId" element={<Product />} />
+          <Route path="*" element={
+            <h1>404 this page doesn't exist</h1>
+          } />
         </Routes>
       </Router>
     )
--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import Product from './Product';
-import { getProducts } from '../store/products/action'
+import { getProducts, getProduct } from '../store/products/action'
 
 class ProductList extends React.Component {
   componentDidMount() {
@@ -13,9 +13,19 @@ class ProductList extends React.Component {
     const { products = [] } = this.props.state;
     return (
       <ul>
-        {products.map(product =>
-          <Product key={product.id} product={product} />
-        )}
+        <h1>Products</h1>
+        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+          {products.map(product =>
+            <Link
+              key={product.id}
+              style={{ width: '400px', height: '400px', border: '1px solid #eee', margin: 5, padding: 5, borderRadius: 5, cursor: 'pointer', color: 'black', textDecoration: 'none' }}
+              to={`/products/${product.id}`}>
+              <img src={product.image} alt={product.title} style={{ width: '200px', height: '200px' }} />
+              <h3>{product.title}</h3>
+              <p>RM {product.price}</p>
+            </Link>
+          )}
+        </div>
       </ul>
     )
   }
@@ -29,7 +39,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    getProducts: () => dispatch(getProducts())
+    getProducts: () => dispatch(getProducts()),
+    getProduct: () => dispatch(getProduct()),
   }
 };
 
